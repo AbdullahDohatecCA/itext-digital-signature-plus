0338088c0ca305a235143c7c8e1cbb31986d8d98
AbdullahCA <abdullah@dohatec.com.bd>
2023-06-13 11:41:16 +0600

    first build for testing

--- src/main/java/com/dohatecca/BaseFrame.java
+++ src/main/java/com/dohatecca/BaseFrame.java
@@ -34,12 +34,12 @@ public class BaseFrame extends JFrame implements ActionListener, MouseListener {
     String signatureImageFilePath;
 
     public BaseFrame() {
-        dohatecLogo = new ImageIcon("src/main/resources/Dohatec.png");
-        dummySignatureImage = new ImageIcon("src/main/resources/DummySignature.png");
-        openIcon = new ImageIcon("src/main/resources/Open.gif");
-        signIcon = new ImageIcon("src/main/resources/Sign.gif");
-        saveIcon = new ImageIcon("src/main/resources/Save.gif");
-        imageIcon = new ImageIcon("src/main/resources/Image.gif");
+        dohatecLogo = new ImageIcon("images/Dohatec.png");
+        dummySignatureImage = new ImageIcon("images/DummySignature.png");
+        openIcon = new ImageIcon("images/Open.gif");
+        signIcon = new ImageIcon("images/Sign.gif");
+        saveIcon = new ImageIcon("images/Save.gif");
+        imageIcon = new ImageIcon("images/Image.gif");
 
         setTitle("Dohatec Digital Signature Tool 2 Demo");
         setIconImage(dohatecLogo.getImage());
